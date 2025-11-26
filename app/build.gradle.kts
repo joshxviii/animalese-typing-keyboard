2e51d83247dc463ad5175b6a67cf9141bdec2a19
Josh <125091065+joshxviii@users.noreply.github.com>
2025-11-26 13:39:25 -0500

    Added androidx text auto size lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.media3.common.ktx)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
