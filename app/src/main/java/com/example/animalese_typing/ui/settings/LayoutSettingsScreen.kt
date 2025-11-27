1b3d17f312b22b446001de6aae188f3ac90e9cbe
Josh <125091065+joshxviii@users.noreply.github.com>
2025-11-27 15:03:43 -0500

    Update LayoutSettingsScreen.kt

--- app/src/main/java/com/example/animalese_typing/ui/settings/LayoutSettingsScreen.kt
+++ app/src/main/java/com/example/animalese_typing/ui/settings/LayoutSettingsScreen.kt
@@ -1,10 +1,16 @@
 package com.example.animalese_typing.ui.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.animalese_typing.AnimalesePreferences
@@ -32,6 +38,16 @@ fun LayoutSettingsScreen(
         RadioButtonItem(title = "QWERTY", selected = true, onClick = {})
         RadioButtonItem(title = "QWERTZ", selected = false, onClick = {})
         RadioButtonItem(title = "AZERTY", selected = false, onClick = {})
+
+        SettingsCategory("Custom Layout")
+        Column (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = { /*TODO*/ }
+            ) { Text(text = "Upload Layout") }
+        }
     }
 }
 
